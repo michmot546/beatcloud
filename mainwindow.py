0791f3b9abd24d14eb3d54b1213eeb42775426cc
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2023-01-13 17:18:55 +0100

    Music player ready

--- mainwindow.py
+++ mainwindow.py
@@ -17,6 +17,7 @@ class App(ctk.CTk):
         ctk.set_default_color_theme("dark-blue")
 
         self.running : bool = True
+        self.song_loaded : bool = False
         self.current_song_length : int = 1
         self.last_song_position : int = 0
         self.position_diff : int = 0
@@ -75,7 +76,7 @@ class App(ctk.CTk):
                                 size=(30, 30))
         self.play_pause_button : ctk.CTkButton = ctk.CTkButton(master=self.control_frame, 
                                     width=30, height=30, image=self.play_image, text="", 
-                                    command=self.play_pause_clicked, fg_color="transparent")
+                                    command=self.play_pause_song, fg_color="transparent")
         self.play_pause_button.grid(row=1, column=2, columnspan=1, sticky="ew")
         #--end
 
@@ -86,7 +87,7 @@ class App(ctk.CTk):
 
         self.next_button : ctk.CTkButton = ctk.CTkButton(master=self.control_frame, 
                                     width=25, height=25, image=self.next_image, text="",
-                                    fg_color="transparent", command=self.next_clicked)
+                                    fg_color="transparent", command=self.next_song)
         self.next_button.grid(row=1, column=3, columnspan=1, sticky="ew")
         #--end
 
@@ -96,7 +97,7 @@ class App(ctk.CTk):
                                   size=(20, 20))
         self.previous_button : ctk.CTkButton = ctk.CTkButton(master=self.control_frame, 
                                     width=25, height=25, image=self.previous_image, text="",
-                                    fg_color="transparent")
+                                    fg_color="transparent", command=self.prev_song)
         self.previous_button.grid(row=1, column=1, columnspan=1, sticky="ew")
         #--end
 
@@ -110,38 +111,55 @@ class App(ctk.CTk):
         self.music_end_label.grid(row=3, column=4, columnspan=1, sticky="ew")
         #--end
 
-        self.test_counter = -1
-
-        self.i = 0
+        self.i = -1
         
     def load_music(self, file : str):
             mixer.init()
             mixer.music.load(file)
-            mixer.music.play()
-            mixer.music.pause()
-            
-            self.song_name = MP3(file).filename
 
+            self.song_name = MP3(file).filename
             self.music_info.configure(text=self.song_name)
 
             song_length : int = math.floor(MP3(file).info.length)
             self.current_song_length = song_length
+
             formated_time : list[str] = get_formated_time(self.current_song_length)
             self.music_end_label.configure(text=(f"{formated_time[1]}:{formated_time[2]}"))
-            self.last_song_position = 0
-            
-    def next_clicked(self):
-        self.load_music(songs[self.i])
-        self.i+=1
 
-    def play_pause_clicked(self):        
+            self.song_loaded = True
+            self.play_pause_button.configure(image=self.pause_image)
+            self.play_pause_state = "PLAY"
+
+            self.song_restart()
+            
+    def next_song(self):
+        if self.i < len(songs) - 1:
+            self.i+=1
+            self.load_music(songs[self.i])
+            
+    def prev_song(self):
+        if self.music_slider.get() < 0.05:
+            if self.i > 0:
+                self.i-=1
+                self.load_music(songs[self.i])
+            else:
+                self.song_restart()
+        else:
+            self.song_restart()
+
+    def song_restart(self):
+        mixer.music.play(start=0)
+        self.last_song_position = 0
+        
+    def play_pause_song(self):     
+        if not self.song_loaded:
+            return
         if self.play_pause_state == "PAUSE":
             self.play_pause_button.configure(image=self.pause_image)
             self.play_pause_state = "PLAY"
 
             self.last_song_position += self.position_diff
-            mixer.music.play(start=self.last_song_position)
-                
+            mixer.music.play(start=self.last_song_position)                
         else:
             self.play_pause_button.configure(image=self.play_image)
             self.play_pause_state = "PAUSE"             
@@ -152,19 +170,27 @@ class App(ctk.CTk):
             mixer.music.pause()
 
     def close(self):
-        self.running = False
-    
+        self.running = False    
 
     def update(self):
         super().update()
-
+        
+        if mixer.get_init():
+            print(mixer.find_channel())
+        
         if self.play_pause_state == "PLAY":
             seconds : float = self.music_slider.get() * self.current_song_length
             formated_time : list[str] = get_formated_time(math.floor(seconds))
             self.music_start_label.configure(text=f"{formated_time[1]}:{formated_time[2]}")
             self.music_slider.set((self.last_song_position + mixer.music.get_pos() / 1000) / self.current_song_length)
 
-    def progress_slider_callback(self, value : float):        
+        if self.music_slider.get() == 1:
+            self.next_song()
+
+    def progress_slider_callback(self, value : float):  
+        if not self.song_loaded:
+            self.music_slider.set(0)
+            return
         self.play_pause_button.configure(image=self.play_image)
         self.play_pause_state = "PAUSE"
         mixer.music.pause()
@@ -177,6 +203,9 @@ class App(ctk.CTk):
         self.music_start_label.configure(text=f"{formated_time[1]}:{formated_time[2]}")
 
     def volume_slider_callback(self, value : float):
+        if not self.song_loaded:
+            self.music_volume_slider.set(0.5)
+            return
         mixer.music.set_volume(value)
 
         
